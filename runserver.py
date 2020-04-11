97bed7c3cd77db1d6c006740cfb8c7bad414d18d
Abhishek <47029841+garbagego@users.noreply.github.com>
2020-04-11 12:58:32 +0530

    added ssl_context

--- runserver.py
+++ runserver.py
@@ -4,7 +4,8 @@ import os
 if __name__ == '__main__':
 
     app.run(
+         ssl_context='adhoc',
          host = '0.0.0.0',
          debug = True,
-         port = 8080
+         port = 8443
     )
