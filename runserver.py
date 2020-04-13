e3d69e5e0aed884e47dcdf7f9b4367d0de57842e
Sudeepa Gorle <sudeepago1994@gmail.com>
2020-04-13 18:16:08 +0530

    reverted to original address

--- runserver.py
+++ runserver.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
     app.run(
        # ssl_context='adhoc',
-         host = 'localhost',
+         host = '0.0.0.0',
          debug = True,
          port = 8080
     )
